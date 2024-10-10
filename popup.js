3f0ac6c53eff8d6e433f85b541e60a134f45228d
iamdpegg <pegg.david.a@gmail.com>
2024-10-10 07:50:20 +0100

    UI changes

--- popup.js
+++ popup.js
@@ -1,22 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const inputText = document.getElementById('input');
-  const outputText = document.getElementById('output');
-  const compressButton = document.getElementById('compress');
-  const copyButton = document.getElementById('copy');
+document.addEventListener("DOMContentLoaded", function () {
+  const inputText = document.getElementById("input");
+  const outputText = document.getElementById("output");
+  const compressButton = document.getElementById("compress");
+  const copyButton = document.getElementById("copy");
 
-  compressButton.addEventListener('click', function() {
-      const text = inputText.value;
-      const cleanedText = text.replace(/\s+/g, ' ').trim();
-      outputText.value = cleanedText;
+  let isCopied = false;
+
+  compressButton.addEventListener("click", function () {
+    const text = inputText.value;
+    const cleanedText = text.replace(/\s+/g, " ").trim();
+    outputText.value = cleanedText;
   });
 
-  copyButton.addEventListener('click', function() {
-      navigator.clipboard.writeText(outputText.value)
-          .then(() => {
-              console.log('Text copied to clipboard');
-          })
-          .catch(err => {
-              console.error('Failed to copy text: ', err);
-          });
+  copyButton.addEventListener("click", function () {
+    navigator.clipboard
+      .writeText(outputText.value)
+      .then(() => {
+        if (!isCopied) {
+          const originalText = copyButton.textContent;
+          copyButton.textContent = "Copied!";
+          isCopied = true;
+
+          setTimeout(() => {
+            copyButton.textContent = originalText;
+            isCopied = false;
+          }, 2000);
+        }
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
   });
 });
